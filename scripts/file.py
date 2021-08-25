e15d0190ed7ae590e988114000b258d3597e06fa
Behigu Gizachew <47280895+behigu@users.noreply.github.com>
2021-08-25 15:16:22 +0300

    update file.py

--- scripts/file.py
+++ scripts/file.py
@@ -1,1 +1,12 @@
+import pandas as pd
+from my_logger import logger
 
+def read_csv(file_name):
+  try:
+    logger.info(f"{file_name} : Reading")
+    null_values = ["n/a", "na", "undefined"]
+    df = pd.read_csv(f'../data/{file_name}', na_values=null_values)
+    logger.info("Reading Files Done!")
+    return df
+  except:
+    logger.error(f"File Not Found : {file_name}")
