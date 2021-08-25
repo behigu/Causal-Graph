92a8ccedea3736dde34cf7b314a5979a4bbca6c6
Behigu Gizachew <47280895+behigu@users.noreply.github.com>
2021-08-25 15:11:12 +0300

    Plotting functions

--- scripts/ploting.py
+++ scripts/ploting.py
@@ -1,1 +1,35 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+def plot_bar(df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabel:str, ax)->None:
+    plt.figure(figsize=(12, 7))
+    sns.barplot(data = df, x=x_col, y=y_col, ax=ax)
+    plt.title(title, size=20)
+    plt.xticks(rotation=75, fontsize=14)
+    plt.yticks( fontsize=14)
+    plt.xlabel(xlabel, fontsize=16)
+    plt.ylabel(ylabel, fontsize=16)
+    plt.show()
+
+def plot_heatmap(df:pd.DataFrame, title:str, cbar=False)->None:
+    plt.figure(figsize=(12, 7))
+    sns.heatmap(df, annot=True, cmap='viridis', vmin=0, vmax=1, fmt='.2f', linewidths=.7, cbar=cbar )
+    plt.title(title, size=18, fontweight='bold')
+    plt.show()
+
+
+def plot_pie(data, column, title:str):
+  plt.figure(figsize=(12, 7))
+  count = data[column].value_counts()
+  colors = sns.color_palette('pastel')[0:5]
+  plt.pie(count, labels = count.index, colors = colors, autopct='%.0f%%')
+  plt.title(title, size=18, fontweight='bold')
+  plt.show()
+
+
+def plot_hist(df:pd.DataFrame, column:str, color:str)->None:
+    plt.figure(figsize=(9, 7))
+    sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
+    plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+    plt.show()
