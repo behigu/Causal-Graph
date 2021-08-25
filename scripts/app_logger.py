22b0eacd274bc7bafa792f99fb2a75ae78624c2d
Behigu Gizachew <47280895+behigu@users.noreply.github.com>
2021-08-25 15:23:08 +0300

    update python logger

--- scripts/app_logger.py
+++ scripts/app_logger.py
@@ -1,1 +1,10 @@
+import logging
 
+#Create and configure logger
+logging.basicConfig(filename="../log/causallogs.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w',
+                    level=logging.DEBUG,
+                   )
+
+logger = logging.getLogger()
